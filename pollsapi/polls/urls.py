6367dcf718fa97e3e089a503e1ece599b8ac381c
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2018-10-17 11:05:09 -0300

    Introducing Viewsets and Routers

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
+from .apiviews import ChoiceList, CreateVote
+from rest_framework.routers import DefaultRouter
+from .views import PollViewSet
+
+router = DefaultRouter()
+router.register('polls', PollViewSet, base_name='polls')
 
 urlpatterns = [
-    path("polls/", PollList.as_view(), name="polls_list"),
-    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
-    path("choices/", ChoiceList.as_view(), name="choice_list"),
-    path("vote/", CreateVote.as_view(), name="create_vote"),
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
 ]
+
+urlpatterns += router.urls
--- pollsapi/polls/views.py
+++ pollsapi/polls/views.py
@@ -1,20 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
+from rest_framework import viewsets
 
-from .models import Poll
-
-def polls_list(request):
-    MAX_OBJECTS = 20
-    polls = Poll.objects.all()[:MAX_OBJECTS]
-    data = {"results": list(polls.values("question", "created_by__username", "pub_date"))}
-    return JsonResponse(data)
+from .models import Poll, Choice
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
 
 
-def polls_detail(request, pk):
-    poll = get_object_or_404(Poll, pk=pk)
-    data = {"results": {
-        "question": poll.question,
-        "created_by": poll.created_by.username,
-        "pub_date": poll.pub_date
-    }}
-    return JsonResponse(data)
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
