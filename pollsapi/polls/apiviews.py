caed9d821033859ccae3c0bea275ff92d1601a57
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2018-10-17 10:29:26 -0300

    Using DRF generic views to simplify code

--- pollsapi/polls/apiviews.py
+++ pollsapi/polls/apiviews.py
@@ -1,19 +1,15 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 from .models import Poll, Choice
-from  .serializers import PollSerializer
-
-class PollList(APIView):
-    def get(self, request):
-        polls = Poll.objects.all()[:20]
-        data = PollSerializer(polls, many=True).data
-        return Response(data)
+from .serializers import PollSerializer, ChoiceSerializer,\
+    VoteSerializer
 
 
-class PollDetail(APIView):
-    def get(self, request, pk):
-        poll = get_object_or_404(Poll, pk=pk)
-        data = PollSerializer(poll).data
-        return Response(data)
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
